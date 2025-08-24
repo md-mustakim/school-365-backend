6273610f7b0ad56f92616fc0aacd5271aac2ba1a
Md Mustakim <mdmustakim.dev@gmail.com>
2025-08-24 18:16:03 +0600

    w

--- migrations/20250816192519_create_class_level_sections.ts
+++ migrations/20250816192519_create_class_level_sections.ts
@@ -1,10 +1,13 @@
-import type { Knex } from "knex";
-
+import type { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable('shift_class_levels', (table) => {
+    table.increments('id');
+    table.string('name').unique();
+    table.timestamps(true, true);
+  });
 }
-
 
 export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable('class_levels');
 }
-
